d8cfd5a45ef0f8a863371c0ae09eea0c518fab02
krishna132022 <106172868+krishna132022@users.noreply.github.com>
2023-05-16 20:45:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+song1='';
+song2='';
+song1_status='';
+somg2_status='';
+scoreRightWrist=0;
+scoreLeftWrist=0;
+rightWristX=0;
+rightWristY=0;
+leftWristX=0;
+leftWristY=0;
+
+function preload(){
+    song1=loadSound("music.mp3");
+    song2-loadSound("music2.mp3");
+}
+
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+image(video,0,0,600,500);
+}
+
+function play(){
+song.play()
+song.setVolume(1)
+song.rate(1)
+}
